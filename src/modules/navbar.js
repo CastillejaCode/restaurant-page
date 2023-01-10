401b68183b1221c76c82a2c45408e95d684b7def
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-01-09 23:26:04 -0800

    Add basic design of home page

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -1,33 +1,11 @@
 export default function insertNavbar() {
 	document.querySelector('#container').insertAdjacentHTML(
 		'afterbegin',
-		`<div class="header">
-    <div class="header-name">
-        <!-- <div>Whiskers</div> -->
-        <!-- <div>and</div> -->
-        <div>
-            Whiskers
-          
-            and
-           
-            Treats
+		`<div class="nav">
+                <div class="nav-1">Home</div>
+                <div class="nav-2">Menu</div>
+                <div class="nav-3">Contact</div>
         </div>
-    </div>
-    <div class="options-container">
-        <div class="options">
-            <div class="nav home">Home</div>
-            <div class="nav menu">Menu</div>
-            <div class="nav contact">Contact</div>
-        </div>
-    </div>
-    <div class="header-name hidden">
-        Whiskers
-        
-        and
-       
-        Treats
-    </div>
-</div>
 `
 	);
 }
